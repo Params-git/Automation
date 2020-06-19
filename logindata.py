54345a954c244a8042584d159d3a10faf35037e4
Paramjeet Singh <singhparamjeet9582@gmail.com>
2020-06-19 23:08:54 +0530

    Add files via upload

---
+++ logindata.py
@@ -0,0 +1,9 @@
+# credentials
+
+NAME     = "name "
+EMAIL    = "email id"
+PASSWORD = "password"
+PHONE    = "phone no."
+ADDRESS  = "home address"
+PINCODE  = "city pincode"
+CITY     = "name of ur city"
