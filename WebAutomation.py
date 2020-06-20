b8cd62a95321d7f73dec119d62a2e7e0d0d12b07
Paramjeet Singh <singhparamjeet9582@gmail.com>
2020-06-21 00:29:32 +0530

    Update WebAutomation.py

--- WebAutomation.py
+++ WebAutomation.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-import logindata
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+import logindata
 
 chromedriver = "E:\chromedriver.exe"
 driver = webdriver.Chrome(chromedriver)
@@ -16,48 +16,52 @@ time.sleep(1)
 driver.get('https://www.amazon.com')
 time.sleep(1)
 
-firstlevelmenu = driver.find_element_by_xpath('//*[@id="nav-link-accountList"]/div/span')
+def XPATH(selector):
+	path = driver.find_element_by_xpath(selector)
+	return path
+
+firstlevelmenu = XPATH('//*[@id="nav-link-accountList"]/div/span')
 action.move_to_element(firstlevelmenu).perform() 
 time.sleep(2)
-secondlevelmenu = driver.find_element_by_xpath('//*[@id="nav-flyout-ya-signin"]/a/span')
+secondlevelmenu = XPATH('//*[@id="nav-flyout-ya-signin"]/a/span')
 secondlevelmenu.click()
 time.sleep(3)
 
-signinelement = driver.find_element_by_xpath('//*[@id="ap_email"]')
+signinelement = XPATH('//*[@id="ap_email"]')
 signinelement.send_keys(logindata.EMAIL)
 time.sleep(4)
 
-cont = driver.find_element_by_xpath('//*[@id="continue"]')
+cont = XPATH('//*[@id="continue"]')
 cont.click()
 time.sleep(3)
 
-password = driver.find_element_by_xpath('//*[@id="ap_password"]')
+password = XPATH('//*[@id="ap_password"]')
 password.send_keys(logindata.PASSWORD)
 time.sleep(4)
 
-login = driver.find_element_by_xpath('//*[@id="signInSubmit"]')
+login = XPATH('//*[@id="signInSubmit"]')
 login.click()
 time.sleep(3)
     
-searchbox = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+searchbox = XPATH('//*[@id="twotabsearchtextbox"]')
 searchbox.send_keys("redmi phone")
 time.sleep(3)
 
-search = driver.find_element_by_xpath('//*[@id="nav-search"]/form/div[2]/div/input')
+search = XPATH('//*[@id="nav-search"]/form/div[2]/div/input')
 search.click()
 time.sleep(3)
 
-product = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[2]/div/span/div/div/div[2]/div[1]/div/div/span/a/div/img').click()
-
+product = XPATH('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[2]/div/span/div/div/div[2]/div[1]/div/div/span/a/div/img').click()
 driver.implicitly_wait(5)
-addToCart = driver.find_element_by_xpath('//*[@id="submit.add-to-cart"]').click()
+
+addToCart = XPATH('//*[@id="submit.add-to-cart"]').click()
 time.sleep(3)
 
 ##driver.implicitly_wait(5)
 ##byNow = driver.find_element_by_xpath('//*[@id="submit.buy-now"]').click()
 ##time.sleep(2)
 
-Cart = driver.find_element_by_xpath('//*[@id="nav-cart"]').click()
+Cart = XPATH('//*[@id="nav-cart"]').click()
 time.sleep(5)
  
 driver.quit()
